ad0853a035285453142acfccccbb86f1b7bfc050
Lahav174 <lahav.lipson@columbia.edu>
2016-07-02 20:12:45 -0400

    Added SwiftCSV pod

--- Vesea_Columbia/Vesea_Columbia/FrontViewController.swift
+++ Vesea_Columbia/Vesea_Columbia/FrontViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftCSV
 
 class FrontViewController: UIViewController {
 
@@ -16,54 +17,12 @@ class FrontViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let originalImage = UIImage(named: "citypic")!
-        let modifiedImage = resizeImage(originalImage, newHeight: self.view.frame.height)
-        let newImage = modifiedImage.CGImage
+        do {
+            let csv = try CSV(name: "users.csv")
+        } catch {
+            // Catch errors or something
+        }
         
-        
-        
-        let croppedImage = CGImageCreateWithImageInRect(newImage, CGRect(x: 0, y: 100, width: self.view.frame.size.width, height: 200))
-        
-        let result = addFilter(croppedImage!)
-        
-        let v = UIImageView(frame: CGRect(x: 0, y: 100, width: self.view.frame.width, height: 200))
-        v.image = result
-        self.view.addSubview(v)
-        
-        
-    }
-    
-//    func resizeImage(image: UIImage, newWidth: CGFloat) -> UIImage {
-//        
-//        let scale = newWidth / image.size.width
-//        let newHeight = image.size.height * scale
-//        UIGraphicsBeginImageContext(CGSizeMake(newWidth, newHeight))
-//        image.drawInRect(CGRectMake(0, 0, newWidth, newHeight))
-//        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext()
-//        
-//        return newImage
-//    }
-
-
-    func resizeImage(image: UIImage, newHeight: CGFloat) -> UIImage {
-        
-        let scale = newHeight / image.size.height
-        let newWidth = image.size.width * scale
-        UIGraphicsBeginImageContext(CGSizeMake(newWidth, newHeight))
-        image.drawInRect(CGRectMake(0, 0, newWidth, newHeight))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return newImage
-    }
-    
-    func addFilter(picture: CGImage) -> UIImage{
-        let beginImage = CIImage(CGImage: picture)
-        let filter = CIFilter(name: "CISepiaTone")
-        filter!.setValue(beginImage, forKey: kCIInputImageKey)
-        filter!.setValue(0.5, forKey: kCIInputIntensityKey)
-        return UIImage(CIImage: (filter?.outputImage)!)
     }
     
 }
